9263c6c0f9e9d50a8d5a5c8d6da0fe5cae0c2fd9
Adeeb Khan <adeebkhankhan25@gmail.com>
2024-10-05 10:26:24 +0530

    Added new project

--- src/cardData.js
+++ src/cardData.js
@@ -1,5 +1,15 @@
 export const cardData = [
   {
+    heading: "AI Music Generator",
+    content:
+      "An AI model that generates original music 🎵 using LSTM-based neural networks.",
+    imgSrc: "/ProjectsPics/AI_Music_Generator.png",
+    tags: ["Machine Learning", "AI", "Tensorflow", "Python", "Neural Networks"],
+    isOnline: true,
+    git: "https://github.com/AdeebKhan25/WeatherApp",
+    access: "https://weatherapp-adeebkhan25s-projects.vercel.app/",
+  },
+  {
     heading: "Messenger",
     content:
       "It is a full-stack, real-time messaging application designed for seamless and intuitive communication.",
